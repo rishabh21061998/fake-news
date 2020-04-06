9722de5c444b630932a364f37d16cb5ea29a9571
rishabh21061998 <49248072+rishabh21061998@users.noreply.github.com>
2020-04-06 10:42:16 +0530

    Update nlp_1.py

--- nlp_1.py
+++ nlp_1.py
@@ -231,66 +231,6 @@ Models['F1'][3] =f1_score(y_test, y_pred_3_2, average="macro")
 
 #******************************************************************************
 #******************************************************************************
-"""
-# *** Applying Deep Learning Technique #1 ***
-
-# LSTM
-
-from keras.models import Model
-from keras.layers import LSTM, Activation, Dense, Dropout, Input, Embedding
-from keras.optimizers import RMSprop
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing import sequence
-from keras.utils import to_categorical
-from keras.callbacks import EarlyStopping
-
-
-
-# RNN
-max_words = 2000
-max_len = 200
-tok = Tokenizer(num_words=max_words)
-tok.fit_on_texts(X_train_svd)
-sequences = tok.texts_to_sequences(X_train_svd)
-sequences_matrix = sequence.pad_sequences(X_train_svd,maxlen=max_len)
-
-def RNN():
-    inputs = Input(name='inputs',shape=[max_len])
-    layer = Embedding(max_words,50,input_length=max_len)(inputs)
-    layer = LSTM(64)(layer)
-    layer = Dense(256,name='FC1')(layer)
-    layer = Activation('relu')(layer)
-    layer = Dropout(0.5)(layer)
-    layer = Dense(1,name='out_layer')(layer)
-    layer = Activation('sigmoid')(layer)
-    model = Model(inputs=inputs,outputs=layer)
-    return model
-
-
-# Call the function and compile the model.
-model = RNN()
-model.summary()
-model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
-
-# Fit on the training data.
-model.fit(X_train_svd_scale,y_train,batch_size=128,epochs=15,
-          validation_split=0.2,callbacks=[EarlyStopping(monitor='val_loss',
-                                                        min_delta=0.0001)])
-
-#  Validation Accuracy: 0.5475
-
-# Process the test set data.
-
-test_sequences_matrix = sequence.pad_sequences(X_test_svd_scale,maxlen=max_len)
-
-# Evaluate the model on the test set
-# Accuracy of the model
-accr = model.evaluate(test_sequences_matrix,y_test)
-print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))       
-
-"""
-#******************************************************************************
-#******************************************************************************
 
 # *** Applying Machine Learning Technique #4 ***
 
